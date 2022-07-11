0dc060efd20618adbf2b2fc7b7ba5ca0d58660e4
Bruno Silva Santana <ibrunoss@outlook.com>
2022-07-10 21:38:56 -0300

    feat: ParkingSpotController created

---
+++ src/main/java/com/api/myfirstapi/controllers/ParkingSpotController.java
@@ -0,0 +1,20 @@
+package com.api.myfirstapi.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.myfirstapi.services.ParkingSpotService;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/parking-spot")
+public class ParkingSpotController {
+    final ParkingSpotService parkingSpotService;
+
+    public ParkingSpotController(ParkingSpotService parkingSpotService) {
+        this.parkingSpotService = parkingSpotService;
+    }
+
+    
+}
