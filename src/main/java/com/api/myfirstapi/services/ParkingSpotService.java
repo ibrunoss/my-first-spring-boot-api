5eb8ac94dbbc2a7ec22dbfa270478fbdc6f2b34f
Bruno Silva Santana <ibrunoss@outlook.com>
2022-07-10 21:38:08 -0300

    chore: remove coments from ParkingSpotService

--- src/main/java/com/api/myfirstapi/services/ParkingSpotService.java
+++ src/main/java/com/api/myfirstapi/services/ParkingSpotService.java
@@ -1,15 +1,11 @@
 package com.api.myfirstapi.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.api.myfirstapi.repositories.ParkingSpotRepository;
 
 @Service
 public class ParkingSpotService {
-    // @Autowired
-    // ParkingSpotRepository parkingSpotRepository;
-
 
     final ParkingSpotRepository parkingSpotRepository;
 
