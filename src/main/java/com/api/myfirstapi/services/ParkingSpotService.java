0226daa72ced4dbe72cdb0891cab769ce7fc848c
Bruno Silva Santana <ibrunoss@outlook.com>
2022-07-14 18:58:02 -0300

    feat: Endpoit to get one parking spot created

--- src/main/java/com/api/myfirstapi/services/ParkingSpotService.java
+++ src/main/java/com/api/myfirstapi/services/ParkingSpotService.java
@@ -1,6 +1,8 @@
 package com.api.myfirstapi.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import javax.transaction.Transactional;
 
@@ -38,4 +40,8 @@ public class ParkingSpotService {
     public List<ParkingSpotModel> findAll() {
         return parkingSpotRepository.findAll();
     }
+
+	public Optional<ParkingSpotModel> findById(UUID id) {
+		return parkingSpotRepository.findById(id);
+	}
 }
