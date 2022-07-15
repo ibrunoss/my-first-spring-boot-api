6134573f8ccb296385e8d0c14bbe14791269c345
Bruno Silva Santana <ibrunoss@outlook.com>
2022-07-14 21:53:25 -0300

    feat: DELETE and UPDATE endpoints created

--- src/main/java/com/api/myfirstapi/controllers/ParkingSpotController.java
+++ src/main/java/com/api/myfirstapi/controllers/ParkingSpotController.java
@@ -12,9 +12,11 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,6 +64,50 @@ public class ParkingSpotController {
     	
     	if (parkingSpotModelOptional.isPresent()) {    		
     		return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
+    	}
+    	
+    	return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found.");
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateParkingSpot(
+    		@PathVariable(value = "id") UUID id,
+    		@RequestBody @Valid ParkingSpotDTO parkingSpotDTO
+	) {
+    	Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+    	
+    	if (parkingSpotModelOptional.isEmpty()) {    	
+        	return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found.");
+    	}
+    	
+//    	Parse manual
+//    	ParkingSpotModel parkingSpotModel = parkingSpotModelOptional.get();
+//    	parkingSpotModel.setApartment(parkingSpotDTO.getApartment());
+//    	parkingSpotModel.setBlock(parkingSpotDTO.getBlock());
+//    	parkingSpotModel.setBrandCar(parkingSpotDTO.getBrandCar());
+//    	parkingSpotModel.setColorCar(parkingSpotDTO.getColorCar());
+//    	parkingSpotModel.setLicensePlateCar(parkingSpotDTO.getLicensePlateCar());
+//    	parkingSpotModel.setModelCar(parkingSpotDTO.getModelCar());
+//    	parkingSpotModel.setParkingSpotNumber(parkingSpotDTO.getParkingSpotNumber());
+//    	parkingSpotModel.setResponsibleName(parkingSpotDTO.getResponsibleName());
+
+//		Parse Auto
+    	ParkingSpotModel parkingSpotModel = new ParkingSpotModel();
+
+    	BeanUtils.copyProperties(parkingSpotDTO, parkingSpotModel);
+    	parkingSpotModel.setId(parkingSpotModelOptional.get().getId());
+    	parkingSpotModel.setRegistrationDate(parkingSpotModelOptional.get().getRegistrationDate());
+    	
+		return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.save(parkingSpotModel));
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteParkingSpot(@PathVariable(value = "id") UUID id) {
+    	Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+    	
+    	if (parkingSpotModelOptional.isPresent()) {
+    		parkingSpotService.delete(parkingSpotModelOptional.get());
+    		return ResponseEntity.status(HttpStatus.OK).body("Parking Spot deleted successfully!");
     	}
     	
     	return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found.");
